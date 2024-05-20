be475ebb96daf9aa6ab86db05cb93316c136b3b7
yasinalkaifi <yasinalkaifi@gmail.com>
2024-05-20 20:10:03 +0500

    frontend-2

---
+++ server/database/db.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+const Connection = () => {
+
+    const MONGODB_URI = `mongodb+srv://l201305:YEpepijpaL7WCS7v@cluster0.00p3juv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected Successfully');
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Database disconnected');
+    })
+
+    mongoose.connection.on('error', () => {
+        console.log('Error while connecting with the database ', error.message);
+    })
+}
+
+export default Connection;
