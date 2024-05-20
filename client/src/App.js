dca5da6fe15c1e1b1e66edf05133c2371e4414c2
yasinalkaifi <yasinalkaifi@gmail.com>
2024-05-20 20:15:25 +0500

    App.js issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
 
 import './App.css';
 
-// components
-import Header from './components/Header';
-import TodoForm from './components/TodoForm';
-import Todos from './components/Todos';
+
 
 function App() {
   return (
     <div>
-      <Header />
-      <TodoForm />
-      <Todos />
     </div>
   );
 }
